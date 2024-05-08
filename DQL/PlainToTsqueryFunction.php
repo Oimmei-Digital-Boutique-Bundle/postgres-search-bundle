3ae7aa4dcacb3a565403cb5e1f2972b3f3696e1c
Ephraim Pepe <ephraim@oimmei.com>
2024-05-08 10:19:45 +0200

    Sostituiti Lexer::* con TokenType::*

--- DQL/PlainToTsqueryFunction.php
+++ DQL/PlainToTsqueryFunction.php
@@ -2,7 +2,7 @@
 namespace Oi\PostgresSearchBundle\DQL;
 
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
-use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\TokenType;
 
 /**
  * TsqueryFunction ::= "PLAINTO_TSQUERY" "(" StringPrimary "," StringPrimary "," StringPrimary ")"
@@ -13,23 +13,23 @@ class PlainToTsqueryFunction extends FunctionNode
     public $queryString = null;
     public $regconfig = null;
 
-    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    public function parse(\Doctrine\ORM\Query\Parser $parser): void
     {
-        $parser->match(Lexer::T_IDENTIFIER);
-        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $parser->match(TokenType::T_IDENTIFIER);
+        $parser->match(TokenType::T_OPEN_PARENTHESIS);
         $this->fieldName = $parser->StringPrimary();
-        $parser->match(Lexer::T_COMMA);
+        $parser->match(TokenType::T_COMMA);
         $this->queryString = $parser->StringPrimary();
 
-        if ($parser->getLexer()->lookahead->type == Lexer::T_COMMA) {
-            $parser->match(Lexer::T_COMMA);
+        if ($parser->getLexer()->lookahead->type == TokenType::T_COMMA) {
+            $parser->match(TokenType::T_COMMA);
             $this->regconfig = $parser->StringPrimary();
         }
 
-        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+        $parser->match(TokenType::T_CLOSE_PARENTHESIS);
     }
 
-    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker): string
     {
         if (!is_null($this->regconfig)) {
             $result =
--- DQL/TsheadlineFunction.php
+++ DQL/TsheadlineFunction.php
@@ -2,7 +2,7 @@
 namespace Oi\PostgresSearchBundle\DQL;
 
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
-use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\TokenType;
 
 /**
  * TsheadlineFunction ::= "TSHEADLINE" "(" StringPrimary "," StringPrimary ")"
@@ -12,17 +12,17 @@ class TsheadlineFunction extends FunctionNode
     public $fieldName = null;
     public $queryString = null;
 
-    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    public function parse(\Doctrine\ORM\Query\Parser $parser): void
     {
-        $parser->match(Lexer::T_IDENTIFIER);
-        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $parser->match(TokenType::T_IDENTIFIER);
+        $parser->match(TokenType::T_OPEN_PARENTHESIS);
         $this->fieldName = $parser->StringPrimary();
-        $parser->match(Lexer::T_COMMA);
+        $parser->match(TokenType::T_COMMA);
         $this->queryString = $parser->StringPrimary();
-        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+        $parser->match(TokenType::T_CLOSE_PARENTHESIS);
     }
 
-    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker): string
     {
         return
             "ts_headline(" . $this->fieldName->dispatch($sqlWalker) . ", " .
